152333de990412b929337c4a0ac8d29d51e0d369
Nikita Shvachko <228nikitalik@gmail.com>
2025-03-10 21:24:44 +0300

    conspekt for 5,6,7,8 lessons

---
+++ lesson5/conspekt.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import title, legend, xlabel, ylabel, subplots_adjust, scatter, errorbar, fill_between, imshow, \
+    colorbar
+
+# plt.show() - запускается только 1 раз
+
+
+#  Ipython
+#  %matplotlib
+# import matplotlib.pyplot as plt
+# plt.plot(...)
+# plt.draw()
+
+
+# Jupyter
+# %matplotlib inline - в блокнот добавляется статический график
+
+# %matplotlib notebook - в блокнот добавляется динамический график
+
+# fig.savefig('filename.png') = сохранение графика
+
+#  output:
+#  matlab plot - в стиле матлаба
+# в оо стиле
+
+
+#  plt.figure(...)
+#  plt.plot(...)  or если несколько графиков plt.subplot(...)
+#  plt.show()
+
+# fig:plt.Figure - объект содержащий (СК тексты метки)
+# axes - система координат для графиков (прямоугольник деления метки)
+
+
+# set_xlim  - границы по оси x
+# set_ylim  - границы по оси y
+# title() - название графика
+# legend( ) - легенда графика
+# xlabel() ylabel( ) - название осей
+
+# subplots_adjust() - сдвиг графика по оси
+# scatter( ) - график с точками вместо линий, если точек больше 1000, то plot предпочтительнее
+
+# errorbar() - график с ошибками (среднее, стандартное отклонение)
+# fill_between() - заливка между двумя графиками
+
+# contour - график с линиями для 3D
+# imshow() -
+# colorbar() -
+
+
